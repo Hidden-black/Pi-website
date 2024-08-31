b2a5b1a2994af776ef66a8209d5946b13dd3c52f
HiddenBlack <68143131+Hidden-black@users.noreply.github.com>
2024-08-31 07:28:48 +0530

    added login

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const fs = require('fs');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const app = express();
-
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 const visitorLogStream = fs.createWriteStream(path.join(__dirname, 'visitors.log'), { flags: 'a' });
+
 const visitorIPs = new Set();
 
 morgan.token('remote-addr', function(req) {
@@ -26,12 +28,41 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(morgan(':remote-addr - :method :url :status :response-time ms', {
-    stream: accessLogStream,
-    immediate: true
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
 }));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const AUTH_CREDENTIALS = { username: 'admin', password: 'password' };
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === AUTH_CREDENTIALS.username && password === AUTH_CREDENTIALS.password) {
+        req.session.authenticated = true;
+        res.redirect('/private');
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+});
+
+function ensureAuthenticated(req, res, next) {
+    if (req.session.authenticated) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/private', ensureAuthenticated, express.static(path.join(__dirname, 'private')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
