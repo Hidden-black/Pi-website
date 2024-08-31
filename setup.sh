e790bde11be3b84e08f4ebdebb92ae7ec8aca474
HiddenBlack <68143131+Hidden-black@users.noreply.github.com>
2024-08-31 12:43:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NGINX_CONFIG_FILE="/etc/nginx/sites-available/pi-website1"
+NGINX_CONFIG_FILE="/etc/nginx/sites-available/pi-website"
 EXPRESS_PORT="5000"
 APP_DIR="/var/www/pi-website"
 GITHUB_REPO="https://github.com/Hidden-black/Pi-website.git"
