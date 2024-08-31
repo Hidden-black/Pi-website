0c35ed4d8e6d5c44f5e766524a4e5a0596c60e0b
HiddenBlack <68143131+Hidden-black@users.noreply.github.com>
2024-08-31 05:59:27 +0530

    Improved logging

--- server.js
+++ server.js
@@ -4,12 +4,32 @@ const morgan = require('morgan');
 const fs = require('fs');
 const app = express();
 
-const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+const visitorLogStream = fs.createWriteStream(path.join(__dirname, 'visitors.log'), { flags: 'a' });
+const visitorIPs = new Set();
+
 morgan.token('remote-addr', function(req) {
     return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 });
 
-app.use(morgan(':remote-addr - :method :url :status :response-time ms', { stream: logStream }));
+function logVisitorIP(ip) {
+    if (!visitorIPs.has(ip)) {
+        visitorIPs.add(ip);
+        visitorLogStream.write(`${ip}\n`);
+    }
+}
+
+app.use((req, res, next) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    logVisitorIP(ip);
+    next();
+});
+
+app.use(morgan(':remote-addr - :method :url :status :response-time ms', {
+    stream: accessLogStream,
+    immediate: true
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
