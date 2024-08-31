77800e5e9e39974be742275e4f6fa039a9790e32
HiddenBlack <68143131+Hidden-black@users.noreply.github.com>
2024-08-31 12:52:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,28 +17,6 @@ pull_git() {
     sudo chown -R $USER:$USER $APP_DIR
 }
 
-configure_nginx() {
-    echo "Configuring Nginx..."
-    sudo tee $NGINX_CONFIG_FILE > /dev/null <<EOL
-server {
-    listen 80;
-    server_name $PUBLIC_IP;
-
-    location / {
-        proxy_pass http://localhost:$EXPRESS_PORT;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-}
-EOL
-    sudo ln -s $NGINX_CONFIG_FILE /etc/nginx/sites-enabled/
-    sudo nginx -t
-    sudo systemctl restart nginx
-    sudo systemctl enable nginx
-}
-
 start() {
     echo "Starting the Express app with PM2..."
     cd $APP_DIR
@@ -49,7 +27,6 @@ start() {
 main() {
     get_public_ip
     pull_git
-    configure_nginx
     start
 
     echo "Complete! Website should be Live at $PUBLIC_IP"
